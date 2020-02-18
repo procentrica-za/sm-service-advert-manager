b10c35a3a4397452808947f2a29f461f8a95a08b
jmaster98 <jo-ash@procentrica.co.za>
2020-02-18 10:56:32 +0200

    Added commenting to handler

--- src/handlers.go
+++ src/handlers.go
@@ -12,16 +12,26 @@ import (
 func (s *Server) handlepostadvertisement() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("handlePostAdvertisement Has Been Called!")
+		//get JSON payload
+
 		postAdvertisement := PostAdvertisement{}
 		err := json.NewDecoder(r.Body).Decode(&postAdvertisement)
+		//handle for bad JSON provided
+
 		if err != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, err.Error())
 			fmt.Println("Could not read body of request into proper JSON format for posting an advertisement.\n Please check that your data is in the correct format.")
 			return
 		}
+
+		//create byte array from JSON payload
 		requestByte, _ := json.Marshal(postAdvertisement)
+
+		//post to crud service
 		req, respErr := http.Post("http://"+config.CRUDHost+":"+config.CRUDPort+"/advertisement", "application/json", bytes.NewBuffer(requestByte))
+
+		//check for response error of 500
 		if respErr != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, respErr.Error())
@@ -45,9 +55,13 @@ func (s *Server) handlepostadvertisement() http.HandlerFunc {
 			return
 		}
 
+		//close the request
 		defer req.Body.Close()
+
+		//create new response struct
 		var postAdvertisementResponse PostAdvertisementResult
 
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(req.Body)
 		err = decoder.Decode(&postAdvertisementResponse)
 		if err != nil {
@@ -56,6 +70,7 @@ func (s *Server) handlepostadvertisement() http.HandlerFunc {
 			fmt.Println("Error occured in decoding post Advertisement response ")
 			return
 		}
+		//convert struct back to JSON
 		js, jserr := json.Marshal(postAdvertisementResponse)
 		if jserr != nil {
 			w.WriteHeader(500)
@@ -63,6 +78,8 @@ func (s *Server) handlepostadvertisement() http.HandlerFunc {
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format! ")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
@@ -71,17 +88,26 @@ func (s *Server) handlepostadvertisement() http.HandlerFunc {
 
 func (s *Server) handleupdateadvertisement() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		//get JSON payload
+
 		updateAdvertisement := UpdateAdvertisement{}
 		err := json.NewDecoder(r.Body).Decode(&updateAdvertisement)
+		//handle for bad JSON provided
+
 		if err != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, err.Error())
 			return
 		}
 		client := &http.Client{}
-		// Create request
+
+		//create byte array from JSON payload
 		requestByte, _ := json.Marshal(updateAdvertisement)
+
+		//post to crud service
 		req, err := http.NewRequest("PUT", "http://"+config.CRUDHost+":"+config.CRUDPort+"/advertisement", bytes.NewBuffer(requestByte))
+
+		//check for response error from CRUD service
 		if err != nil {
 			fmt.Fprint(w, err.Error())
 			fmt.Println("Error in communication with CRUD service endpoint for request to update advertisement")
@@ -109,11 +135,17 @@ func (s *Server) handleupdateadvertisement() http.HandlerFunc {
 			fmt.Println("Request to DB can't be completed with request: " + bodyString)
 			return
 		}
+
+		//close the request
 		defer resp.Body.Close()
 
+		//create new response struct
 		var updateAdvertisementResponse UpdateAdvertisementResult
+
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(resp.Body)
 		err = decoder.Decode(&updateAdvertisementResponse)
+		//handle for bad JSON provided
 		if err != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, err.Error())
@@ -121,12 +153,15 @@ func (s *Server) handleupdateadvertisement() http.HandlerFunc {
 			return
 		}
 		js, jserr := json.Marshal(updateAdvertisementResponse)
+		//convert struct back to JSON
 		if jserr != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, jserr.Error())
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
@@ -135,6 +170,7 @@ func (s *Server) handleupdateadvertisement() http.HandlerFunc {
 
 func (s *Server) handledeleteuseradvertisements() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		//Get User ID from URL
 		userid := r.URL.Query().Get("id")
 		if userid == "" {
 			w.WriteHeader(500)
@@ -143,7 +179,11 @@ func (s *Server) handledeleteuseradvertisements() http.HandlerFunc {
 			return
 		}
 		client := &http.Client{}
+
+		//post to crud service
 		req, respErr := http.NewRequest("DELETE", "http://"+config.CRUDHost+":"+config.CRUDPort+"/useradvertisements?id="+userid, nil)
+
+		//check for response error of 500
 		if respErr != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, respErr.Error())
@@ -156,6 +196,8 @@ func (s *Server) handledeleteuseradvertisements() http.HandlerFunc {
 			fmt.Fprint(w, err.Error())
 			return
 		}
+
+		//close the request
 		defer resp.Body.Close()
 		if resp.StatusCode != 200 {
 			w.WriteHeader(resp.StatusCode)
@@ -173,15 +215,21 @@ func (s *Server) handledeleteuseradvertisements() http.HandlerFunc {
 			fmt.Println("Request to DB can't be completed with request: " + bodyString)
 			return
 		}
+
+		//create new response struct
 		var deleteUserAdvertisementResponse DeleteUserAdvertisementResult
+
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(resp.Body)
 		err = decoder.Decode(&deleteUserAdvertisementResponse)
+		//handle for bad Response recieved from CRUD service
 		if err != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, err.Error())
 			fmt.Println("Error occured in decoding delete Advertisement response ")
 			return
 		}
+		//convert struct back to JSON
 		js, jserr := json.Marshal(deleteUserAdvertisementResponse)
 		if jserr != nil {
 			w.WriteHeader(500)
@@ -189,6 +237,8 @@ func (s *Server) handledeleteuseradvertisements() http.HandlerFunc {
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
@@ -197,7 +247,11 @@ func (s *Server) handledeleteuseradvertisements() http.HandlerFunc {
 
 func (s *Server) handleremoveadvertisement() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		//Get Advertisement ID from URL
 		advertisementid := r.URL.Query().Get("id")
+
+		//Check if Advertisement ID is null
 		if advertisementid == "" {
 			w.WriteHeader(500)
 			fmt.Fprint(w, "AdvertisementID not properly provided in URL")
@@ -205,8 +259,12 @@ func (s *Server) handleremoveadvertisement() http.HandlerFunc {
 			return
 		}
 		client := &http.Client{}
+
+		//post to crud service
 		req, respErr := http.NewRequest("DELETE", "http://"+config.CRUDHost+":"+config.CRUDPort+"/advertisement?id="+advertisementid, nil)
 		if respErr != nil {
+
+			//check for response error of 500
 			w.WriteHeader(500)
 			fmt.Fprint(w, respErr.Error())
 			fmt.Println("Error in communication with CRUD service endpoint for request to delete an advertisement")
@@ -218,6 +276,8 @@ func (s *Server) handleremoveadvertisement() http.HandlerFunc {
 			fmt.Fprint(w, err.Error())
 			return
 		}
+
+		//close the request
 		defer resp.Body.Close()
 		if resp.StatusCode != 200 {
 			w.WriteHeader(resp.StatusCode)
@@ -235,7 +295,11 @@ func (s *Server) handleremoveadvertisement() http.HandlerFunc {
 			fmt.Println("Request to DB can't be completed with request: " + bodyString)
 			return
 		}
+
+		//create new response struct
 		var deleteAdvertisementResponse DeleteAdvertisementResult
+
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(resp.Body)
 		err = decoder.Decode(&deleteAdvertisementResponse)
 		if err != nil {
@@ -244,6 +308,7 @@ func (s *Server) handleremoveadvertisement() http.HandlerFunc {
 			fmt.Println("Error occured in decoding delete Advertisement response ")
 			return
 		}
+		//convert struct back to JSON
 		js, jserr := json.Marshal(deleteAdvertisementResponse)
 		if jserr != nil {
 			w.WriteHeader(500)
@@ -251,6 +316,8 @@ func (s *Server) handleremoveadvertisement() http.HandlerFunc {
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
@@ -259,7 +326,11 @@ func (s *Server) handleremoveadvertisement() http.HandlerFunc {
 
 func (s *Server) handlegetuseradvertisements() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		//Get User ID from URL
+
 		userid := r.URL.Query().Get("id")
+
+		//Check if User ID provided is null
 		if userid == "" {
 			w.WriteHeader(500)
 			fmt.Fprint(w, "User ID not properly provided in URL")
@@ -267,6 +338,8 @@ func (s *Server) handlegetuseradvertisements() http.HandlerFunc {
 			return
 		}
 		req, respErr := http.Get("http://" + config.CRUDHost + ":" + config.CRUDPort + "/useradvertisements?id=" + userid)
+
+		//check for response error of 500
 		if respErr != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, respErr.Error())
@@ -289,11 +362,15 @@ func (s *Server) handlegetuseradvertisements() http.HandlerFunc {
 			fmt.Println("An internal error has occured whilst trying to get a users advertisement data" + bodyString)
 			return
 		}
+
+		//close the request
 		defer req.Body.Close()
 
+		//create new response struct for JSON list
 		getUserAdvertisementResponse := UserAdvertisementList{}
 		getUserAdvertisementResponse.UserAdvertisements = []GetUserAdvertisementResult{}
 
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(req.Body)
 		err := decoder.Decode(&getUserAdvertisementResponse)
 		if err != nil {
@@ -302,6 +379,7 @@ func (s *Server) handlegetuseradvertisements() http.HandlerFunc {
 			fmt.Println("Error occured in decoding get User Advertisement response ")
 			return
 		}
+		//convert struct back to JSON
 		js, jserr := json.Marshal(getUserAdvertisementResponse)
 		if jserr != nil {
 			w.WriteHeader(500)
@@ -309,6 +387,8 @@ func (s *Server) handlegetuseradvertisements() http.HandlerFunc {
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
@@ -317,14 +397,22 @@ func (s *Server) handlegetuseradvertisements() http.HandlerFunc {
 
 func (s *Server) handlegetadvertisementbytype() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		//Get Advertisement type from URL
+
 		advertisementtype := r.URL.Query().Get("adverttype")
+
+		//Check if no Advertisement type was provided in the URL
 		if advertisementtype == "" {
 			w.WriteHeader(500)
 			fmt.Fprint(w, "AdvertisementID not properly provided in URL")
 			fmt.Println("AdvertisementID not properly provided in URL")
 			return
 		}
+
+		//post to crud service
 		req, respErr := http.Get("http://" + config.CRUDHost + ":" + config.CRUDPort + "/advertisementtype?adverttype=" + advertisementtype)
+
+		//check for response error of 500
 		if respErr != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, respErr.Error())
@@ -347,9 +435,13 @@ func (s *Server) handlegetadvertisementbytype() http.HandlerFunc {
 			fmt.Println("An internal error has occured whilst trying to get advertisement data" + bodyString)
 			return
 		}
+
+		//close the request
 		defer req.Body.Close()
 		getTypeAdvertisementResponse := TypeAdvertisementList{}
 		getTypeAdvertisementResponse.TypeAdvertisements = []GetAdvertisementsResult{}
+
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(req.Body)
 		err := decoder.Decode(&getTypeAdvertisementResponse)
 		if err != nil {
@@ -358,6 +450,7 @@ func (s *Server) handlegetadvertisementbytype() http.HandlerFunc {
 			fmt.Println("Error occured in decoding get Advertisement response ")
 			return
 		}
+		//convert struct back to JSON
 		js, jserr := json.Marshal(getTypeAdvertisementResponse)
 		if jserr != nil {
 			w.WriteHeader(500)
@@ -365,6 +458,8 @@ func (s *Server) handlegetadvertisementbytype() http.HandlerFunc {
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
@@ -373,14 +468,22 @@ func (s *Server) handlegetadvertisementbytype() http.HandlerFunc {
 
 func (s *Server) handlegetadvertisement() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		//Get Advertisement ID from URL
+
 		advertisementid := r.URL.Query().Get("id")
+
+		//Check if no Advertisement ID was provided in the URL
 		if advertisementid == "" {
 			w.WriteHeader(500)
 			fmt.Fprint(w, "AdvertisementID not properly provided in URL")
 			fmt.Println("AdvertisementID not properly provided in URL")
 			return
 		}
+
+		//post to crud service
 		req, respErr := http.Get("http://" + config.CRUDHost + ":" + config.CRUDPort + "/advertisement?id=" + advertisementid)
+
+		//check for response error of 500
 		if respErr != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, respErr.Error())
@@ -403,8 +506,14 @@ func (s *Server) handlegetadvertisement() http.HandlerFunc {
 			fmt.Println("An internal error has occured whilst trying to get advertisement data" + bodyString)
 			return
 		}
+
+		//close the request
 		defer req.Body.Close()
+
+		//create new response struct
 		var getAdvertisementResponse GetAdvertisementResult
+
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(req.Body)
 		err := decoder.Decode(&getAdvertisementResponse)
 		if err != nil {
@@ -413,6 +522,8 @@ func (s *Server) handlegetadvertisement() http.HandlerFunc {
 			fmt.Println("Error occured in decoding get Advertisement response ")
 			return
 		}
+
+		//convert struct back to JSON
 		js, jserr := json.Marshal(getAdvertisementResponse)
 		if jserr != nil {
 			w.WriteHeader(500)
@@ -420,6 +531,8 @@ func (s *Server) handlegetadvertisement() http.HandlerFunc {
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
@@ -428,7 +541,11 @@ func (s *Server) handlegetadvertisement() http.HandlerFunc {
 
 func (s *Server) handlegetalladvertisements() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		//Get request from URL
+		//post to crud service
 		req, respErr := http.Get("http://" + config.CRUDHost + ":" + config.CRUDPort + "/advertisements")
+		//handle for bad Response recieved from CRUD service
+		//check for response error of 500
 		if respErr != nil {
 			w.WriteHeader(500)
 			fmt.Fprintf(w, respErr.Error())
@@ -450,9 +567,15 @@ func (s *Server) handlegetalladvertisements() http.HandlerFunc {
 			fmt.Println("An internal error has occured whilst trying to get advertisement data" + bodyString)
 			return
 		}
+
+		//close the request
 		defer req.Body.Close()
 		getAdvertisementList := AdvertisementList{}
+
+		//create new response struct for JSON list
 		getAdvertisementList.Advertisements = []GetAdvertisementsResult{}
+
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(req.Body)
 		err := decoder.Decode(&getAdvertisementList)
 		if err != nil {
@@ -461,6 +584,8 @@ func (s *Server) handlegetalladvertisements() http.HandlerFunc {
 			fmt.Println("Error occured in decoding get Advertisement response ")
 			return
 		}
+
+		//convert struct back to JSON
 		js, jserr := json.Marshal(getAdvertisementList)
 		if jserr != nil {
 			w.WriteHeader(500)
@@ -468,6 +593,8 @@ func (s *Server) handlegetalladvertisements() http.HandlerFunc {
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
@@ -476,14 +603,22 @@ func (s *Server) handlegetalladvertisements() http.HandlerFunc {
 
 func (s *Server) handlegetadvertisementbyposttype() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
+		//Get Ad post type from URL
 		advertisementposttype := r.URL.Query().Get("advertposttype")
+
+		//Check if Advertisement Post Type is not provided in URL
 		if advertisementposttype == "" {
 			w.WriteHeader(500)
 			fmt.Fprint(w, "Post type not properly provided in URL")
 			fmt.Println("Post type not properly provided in URL")
 			return
 		}
+
+		//post to crud service
 		req, respErr := http.Get("http://" + config.CRUDHost + ":" + config.CRUDPort + "/advertisementposttype?advertposttype=" + advertisementposttype)
+
+		//check for response error of 500
 		if respErr != nil {
 			w.WriteHeader(500)
 			fmt.Fprint(w, respErr.Error())
@@ -506,9 +641,15 @@ func (s *Server) handlegetadvertisementbyposttype() http.HandlerFunc {
 			fmt.Println("An internal error has occured whilst trying to get advertisement data" + bodyString)
 			return
 		}
+
+		//close the request
 		defer req.Body.Close()
+
+		//create new response struct for JSON list
 		getTypeAdvertisementResponse := TypeAdvertisementList{}
 		getTypeAdvertisementResponse.TypeAdvertisements = []GetAdvertisementsResult{}
+
+		//decode request into decoder which converts to the struct
 		decoder := json.NewDecoder(req.Body)
 		err := decoder.Decode(&getTypeAdvertisementResponse)
 		if err != nil {
@@ -517,6 +658,7 @@ func (s *Server) handlegetadvertisementbyposttype() http.HandlerFunc {
 			fmt.Println("Error occured in decoding get Advertisement response ")
 			return
 		}
+		//convert struct back to JSON
 		js, jserr := json.Marshal(getTypeAdvertisementResponse)
 		if jserr != nil {
 			w.WriteHeader(500)
@@ -524,6 +666,8 @@ func (s *Server) handlegetadvertisementbyposttype() http.HandlerFunc {
 			fmt.Println("Error occured when trying to marshal the decoded response into specified JSON format!")
 			return
 		}
+
+		//return success back to Front-End user
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
 		w.Write(js)
